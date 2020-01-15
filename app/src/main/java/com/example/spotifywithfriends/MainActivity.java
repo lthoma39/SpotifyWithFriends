5db873185414c9393484169ff1f46cd14dab4783
Latrell <lthoma39@uic.edu>
2020-01-15 06:05:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/spotifywithfriends/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.spotifywithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, SpotifyMode.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/spotifywithfriends/SpotifyMode.java
@@ -0,0 +1,14 @@
+package com.example.spotifywithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotifyMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_mode);
+    }
+}
